d51fd97da4cb74c8b5c4476f05a6eb919c807784
jacobmodayil <jacobmodayil@outlook.com>
2015-09-19 16:09:30 -0500

    part 1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,8 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @sort = params[:sort]
+    @movies = Movie.order(params[:sort])
   end
 
   def new
